ccdc7de2b1c6a89678c8e65f4e0f536f847377eb
gaodewei <gaodewei@ztzd.com>
2019-09-24 10:16:40 +0800

    测试

--- src/main/java/com/yquick/simp/controller/RuiTaiController.java
+++ src/main/java/com/yquick/simp/controller/RuiTaiController.java
@@ -1,6 +1,7 @@
 package com.yquick.simp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +44,11 @@ public class RuiTaiController {
 	@PostMapping("/sendEmail")
 	public Object sendEmail(@RequestBody JSONObject data) {
 		return ruiTaiService.sendEmail(data);
+	}
+
+	@GetMapping("/test")
+	public String test() {
+		return "just test";
 	}
 
 }
